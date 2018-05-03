bbfab0a80e6bd84a988f2822b0766ef3dae7afd8
donrestarone <shashikejayatunge@gmail.com>
2018-05-03 15:48:04 -0400

    finish exercise

--- backpack.rb
+++ backpack.rb
@@ -13,43 +13,65 @@ class Backpack
     # set up local variables used by rest of prepare method
     weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
-    @items << 'pants'
-    @items << 'shirt'
+
     # Ensure appropriate clothing is added to backpack
-
+    default_items
     if weather == 'rainy'
-      @items << 'umbrella'
+      rainy_weather_items
     elsif weather == 'cold'
-      @items << 'jacket'
+      cold_weather_items
     end
-
     # Ensure gym shoes are added to backpack if it's a gym day
     # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
-      @items << 'gym shoes'
+      gym_items
     end
 
     # Bring a packed lunch on all weekdays
     if day_of_week != 'saturday' && day_of_week != 'sunday'
-      @items << 'packed lunch'
-    elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
-      @items << 'snacks'
+      pack_lunch
     end
+  end
+
+  def pack_lunch
+    @items << 'packed lunch'
+  end
+
+  def gym_items
+    @items << 'gym shoes'
+  end
+
+  def default_items
+    @items << 'pants'
+    @items << 'shirt'
+  end
+
+  def cold_weather_items
+    @items << 'jacket'
+  end
+
+  def rainy_weather_items
+    @items << 'umbrella'
   end
 
   # Prints a summary packing list for Melinda's backpack
-  def my_func
+  def summary_of_packing_list
     output = []
-    output << "Melinda, here's your packing list!"
-    output << "Day: #{@attributes[:day_of_week]}, Weather: #{@attributes[:weather]}"
-    output << ""
-
-    @items.each do |item|
-      output << "- #{item}"
-    end
+    information(output)
+    pack_items(output)
     output.join("\n")
   end
 
+  def information(array)
+    array << "Melinda, here's your packing list!"
+    array << "Day: #{@attributes[:day_of_week]}, Weather: #{@attributes[:weather]}"
+    array << ""
+  end
+
+  def pack_items(output)
+    @items.each do |item|
+      output << "- #{item}"
+    end
+  end
 end
